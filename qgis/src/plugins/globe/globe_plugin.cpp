269ac6a87a88f434adbd2ce3aedb035977a88b5a
Pirmin Kalberer <pka@sourcepole.ch>
2010-07-12 10:55:00 +0200

    Globe plugin skeleton added

---
+++ qgis/src/plugins/globe/globe_plugin.cpp
@@ -0,0 +1,117 @@
+/***************************************************************************
+    globe_plugin.cpp
+
+    Globe Plugin
+    a QGIS plugin
+     --------------------------------------
+    Date                 : 08-Jul-2010
+    Copyright            : (C) 2010 by Sourcepole
+    Email                : info at sourcepole.ch
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "globe_plugin.h"
+#include "globe_plugin_gui.h"
+
+#include <qgisinterface.h>
+#include <qgisgui.h>
+
+#include <QAction>
+#include <QToolBar>
+
+//static const char * const sIdent = "$Id: plugin.cpp 9327 2008-09-14 11:18:44Z jef $";
+static const QString sName = QObject::tr( "Globe" );
+static const QString sDescription = QObject::tr( "Overlay data on a 3D globe" );
+static const QString sPluginVersion = QObject::tr( "Version 0.1" );
+static const QgisPlugin::PLUGINTYPE sPluginType = QgisPlugin::UI;
+
+GlobePlugin::GlobePlugin( QgisInterface* theQgisInterface )
+  : QgisPlugin( sName, sDescription, sPluginVersion, sPluginType ),
+    mQGisIface( theQgisInterface ),
+    mQActionPointer( NULL )
+{
+}
+
+GlobePlugin::~GlobePlugin()
+{
+}
+
+void GlobePlugin::initGui()
+{
+  // Create the action for tool
+  mQActionPointer = new QAction( QIcon( ":/globe/globe_plugin.png" ), tr( "Globe" ), this );
+  // Set the what's this text
+  mQActionPointer->setWhatsThis( tr( "Overlay data on a 3D globe" ) );
+  // Connect the action to the run
+  connect( mQActionPointer, SIGNAL( triggered() ), this, SLOT( run() ) );
+  // Add the icon to the toolbar
+  mQGisIface->addToolBarIcon( mQActionPointer );
+  mQGisIface->addPluginToMenu( tr( "&Globe" ), mQActionPointer );
+}
+
+void GlobePlugin::run()
+{
+  QgsGlobePluginGui *myPluginGui = new QgsGlobePluginGui( mQGisIface->mainWindow(), QgisGui::ModalDialogFlags );
+  myPluginGui->setAttribute( Qt::WA_DeleteOnClose );
+
+  myPluginGui->show();
+}
+
+void GlobePlugin::unload()
+{
+  // remove the GUI
+  mQGisIface->removePluginMenu( "&Globe", mQActionPointer );
+  mQGisIface->removeToolBarIcon( mQActionPointer );
+  delete mQActionPointer;
+}
+
+void GlobePlugin::help()
+{
+}
+
+/**
+ * Required extern functions needed  for every plugin
+ * These functions can be called prior to creating an instance
+ * of the plugin class
+ */
+// Class factory to return a new instance of the plugin class
+QGISEXTERN QgisPlugin * classFactory( QgisInterface * theQgisInterfacePointer )
+{
+  return new GlobePlugin( theQgisInterfacePointer );
+}
+// Return the name of the plugin - note that we do not user class members as
+// the class may not yet be insantiated when this method is called.
+QGISEXTERN QString name()
+{
+  return sName;
+}
+
+// Return the description
+QGISEXTERN QString description()
+{
+  return sDescription;
+}
+
+// Return the type (either UI or MapLayer plugin)
+QGISEXTERN int type()
+{
+  return sPluginType;
+}
+
+// Return the version number for the plugin
+QGISEXTERN QString version()
+{
+  return sPluginVersion;
+}
+
+// Delete ourself
+QGISEXTERN void unload( QgisPlugin * thePluginPointer )
+{
+  delete thePluginPointer;
+}
---
+++ qgis/src/plugins/globe/globe_plugin.h
@@ -0,0 +1,55 @@
+/***************************************************************************
+    globe.h
+
+    Globe Plugin
+    a QGIS plugin
+     --------------------------------------
+    Date                 : 08-Jul-2010
+    Copyright            : (C) 2010 by Sourcepole
+    Email                : info at sourcepole.ch
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef QGS_GLOBE_PLUGIN_H
+#define QGS_GLOBE_PLUGIN_H
+
+#include "../qgisplugin.h"
+#include <QObject>
+
+class QAction;
+class QToolBar;
+class QgisInterface;
+
+class GlobePlugin : public QObject, public QgisPlugin
+{
+  Q_OBJECT
+
+  public:
+    GlobePlugin( QgisInterface* theQgisInterface );
+    virtual ~GlobePlugin();
+
+  public slots:
+    //! init the gui
+    virtual void initGui();
+    //! Show the dialog box
+    void run();
+    //! unload the plugin
+    void unload();
+    //! show the help document
+    void help();
+
+  private:
+    int mPluginType;
+    //! Pointer to the QGIS interface object
+    QgisInterface *mQGisIface;
+    //!pointer to the qaction for this plugin
+    QAction * mQActionPointer;
+};
+
+#endif // QGS_GLOBE_PLUGIN_H
